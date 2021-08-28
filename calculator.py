5e8e281fc7a0ab6cecb4d78812df843d11fd2813
kinsleywhite9 <kinsleywhite9@gmail.com>
2021-08-28 14:47:12 +0000

    fixed if choice in statement to add 3 & 4

--- calculator.py
+++ calculator.py
@@ -19,7 +19,7 @@ print("4. Divide")
 while True:
     choice = input("Enter Choice (1,2,3,4): ")
     
-    if choice in ('1', '2'):
+    if choice in ('1', '2','3','4'):
         num1 = float(input("Enter a number: "))
         num2 = float(input("Enter a second number: "))
 
