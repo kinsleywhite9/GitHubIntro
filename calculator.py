ec57e8b27f66af3e255537dcb88e3705374def76
kinsleywhite9 <kinsleywhite9@gmail.com>
2021-08-28 14:31:04 +0000

    added add and subtract from calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+print("Select Operations")
+print("1. Add")
+print("2. Subtract")
+
+while True:
+    choice = input("Enter Choice (1,2,3,4): ")
+    if choice in ('1', '2'):
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter a second number: "))
+
+        if choice == "1":
+            print(num1, "+" ,num2 ,"=" ,add(num1,num2))
+        break 
+    else:
+        print("invalid input") 
+       
